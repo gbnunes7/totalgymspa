f878969f17964519604a80dbcd874d3f98c7376d
Gabriel Melo <bielrj170@gmail.com>
2024-09-14 22:41:24 -0300

    style: change style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
 	return (
 		<header className="fixed z-10 w-full flex min-h-[60px] bg-transparent justify-between px-[40px] py-[10px] items-center">
 			<nav>
-				<ul className="flex bg-[#8080809E] gap-4 p-4 rounded-xl text-base text-white">
+				<ul className="flex bg-[#8080809E] gap-4 px-3.5 py-2 rounded-xl text-base text-white">
 					<ListItem>Home</ListItem>
 					<ListItem>Planos</ListItem>
 					<ListItem>Aulas</ListItem>
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
 			<img
 				src="/logo.png"
 				alt="Logo da TotalGym"
-				className="w-[100px] md:mr-[250px]"
+				className="w-[80px] md:mr-[250px] relative top-2"
 			/>
 			<button className="bg-white p-1.5 rounded-lg">
 				<IoMdMenu className="text-3xl" />
