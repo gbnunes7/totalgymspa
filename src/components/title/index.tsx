093683e5d8bf2745dba6c1e1ae560e73920744e7
Gabriel Melo <bielrj170@gmail.com>
2024-09-14 22:41:28 -0300

    style: change style

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,6 +1,6 @@
 const Title = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<h2 className="text-6xl font-bold text-orange-500 uppercase">{children}</h2>
+		<h2 className="text-5xl font-bold text-orange-500 uppercase">{children}</h2>
 	);
 };
 
