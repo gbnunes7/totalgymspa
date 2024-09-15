0d9c4ac2503eb2cbddbbf279d857087a95216da0
Gabriel Melo <bielrj170@gmail.com>
2024-09-14 22:41:50 -0300

    feat: add a new section to UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaArrowRight } from "react-icons/fa";
+import BannerPrincipal from "./components/banner";
+import Header from "./components/header";
+import Title from "./components/title";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="min-h-screen flex flex-col bg-[#212121]">
+			<Header />
+			<BannerPrincipal />
+			<section className="w-full flex p-16 gap-36">
+				<div className="flex flex-col gap-16">
+					<Title>
+						Atividades físicas para<br></br> toda a família
+					</Title>
+					<p className="text-white text-lg">
+						Oferecemos mais de 15 opções de atividades! Enquanto seu filho
+						aproveita a natação, você pode participar de aulas como spinning,
+						ginástica localizada ou até mesmo artes marciais como Muay Thai. Se
+						preferir, também pode se exercitar em nossa área de musculação.
+					</p>
+					<button className="text-black flex items-center justify-between bg-orange-400 w-40 px-4 py-2 rounded-lg">
+						Saber mais
+						<div className="bg-white rounded-sm p-1">
+							<FaArrowRight className="text-xl" />
+						</div>
+					</button>
+				</div>
+				<img src="/academia.png" alt="" className="w-2/4 rounded-2xl" />
+			</section>
+		</div>
+	);
 }
 
-export default App
+export default App;
